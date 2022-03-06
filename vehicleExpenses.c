4a78ae191baf42e9cfea8e87804b8292a2986f43
Conner <clramirezxr1999@gmail.com>
2022-03-05 19:52:11 -0800

    Added do while loopa

--- vehicleExpenses.c
+++ vehicleExpenses.c
@@ -17,36 +17,75 @@ int main() {
 
 float airfare, rental, miles, fees, taxi; 
 
-
-printf("Total Spent on Airfare: $");
+do
+{
+ printf("Total Spent on Airfare: $");
 scanf("%f",&airfare);
 
-printf("Total Spent on Car Rental: $");
+if (airfare < 0)
+printf("Please enter a Positive Number!\n");
+
+} while (airfare < 0);
+
+do
+{
+ printf("Total Spent on Car Rental: $");
 scanf("%f",&rental);
 
-printf("Total Miles Driven: $");
+if (rental < 0)
+printf("Please enter a Positive Number!\n");
+
+} while (rental < 0);
+
+do
+{
+ printf("Total Miles Driven: $");
 scanf("%f",&miles);
 
+if (miles < 0)
+printf("Please enter a Positive Number!\n");
+
+} while (miles < 0);
+
+do
+{
 printf("Total Spent on Parking: $");
 scanf("%f",&fees);
 
+if (fees < 0)
+printf("Please enter a Positive Number!\n");
+
+} while (fees < 0);
+
+do
+{
 printf("Total Spent on Taxi': $");
 scanf("%f",&taxi);
 
-//while(airfare < 0 )
-  // printf("Please provide a positive value.");
+if (taxi < 0)
+printf("Please enter a Positive Number!\n");
+
+} while (taxi < 0);
+
   
 float mTotal;
 
 mTotal = miles * 0.27; 
 
-printf("-------------------------------------------");
+printf("-------------------------------------------\n");
 
-printf("Total Airfare: $%.2f",airfare);
-printf("Total Car Rental: $%.2f",rental);
-printf("Total Miles Cost: $%.2f",mTotal);
-printf("Total Parking Fees: $%.2f",fees);
-printf("Total Taxi Cost: $%.2f",taxi);
+printf("\nTotal Airfare: $%.2f",airfare);
+printf("\nTotal Car Rental: $%.2f",rental);
+printf("\nTotal Miles Cost: $%.2f",mTotal);
+printf("\nTotal Parking Fees: $%.2f",fees);
+
+if ( fees > 6 )
+    printf( " Parking Fees over $6 a day you must pay out of pocket." );
+
+printf("\nTotal Taxi Cost: $%.2f",taxi);
+
+if ( taxi > 10 )
+    printf( " Taxi Fees over $10 a day you must pay out of pocket." );
 
 return 0;
 }
