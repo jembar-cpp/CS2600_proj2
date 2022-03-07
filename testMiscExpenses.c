91d5bd468454a730d4accf5d904fbb4031ae0c0d
rctabares <98509950+rctabares@users.noreply.github.com>
2022-03-07 14:20:04 -0800

    edited unit test file

--- testMiscExpenses.c
+++ testMiscExpenses.c
@@ -12,19 +12,11 @@ void testHotelFees()
 {
 
     // Declare variables needed
-    float totalSaved, totalExcess, totalHotelCost, allowedHotelCost, testCost;
+    float totalHotelCost, allowedHotelCost, testCost;
     float nights = 5;
     float hotelFee = 85.0;
     float semFee = 150.0; 
     float allowedPerNight = 90;
-
-    // Asks user for input of hotel fee per night, and total amount of nights at hotel
-    printf("Enter the hotel fee per night: $");
-    printf("%.2f", hotelFee);
-    printf("Enter total nights at hotel: ");
-    printf("%.2f", nights);
-    printf("Enter total of seminar/conference fees: $");
-    printf("%,2f", semFee);
 
     // Calculates costs of both allowed total and total businessperson spent
     totalHotelCost = hotelFee * nights;
@@ -34,27 +26,17 @@ void testHotelFees()
     // Checks for hotelFee only, so rest of calcs are easier to check 
     if (hotelFee <= 89.99)
     {
-        testCost = allowedHotelCost - totalHotelCost;
-        printf("total cost is less than allowed, so we saved: $%.2f\n", testCost);
+        testCost = (allowedHotelCost - totalHotelCost) + semFee;
     }
     else if (hotelFee >= 90.99)
     {
-        testCost = totalHotelCost - allowedHotelCost;
-        printf("total cost is more than allowed, so we over spent: $%.2f\n", (totalHotelCost - allowedHotelCost));
+        testCost = fabs(totalHotelCost - allowedHotelCost) + semFee;
     }
     else
     {  
-        testCost = 0.0;
-        printf("The company gave you the exact amount you needed! (for hotels): $%.2f\n", testCost);
+        testCost = semFee;
     }
 
-    TEST_ASSERT_EQUAL(testCost, 25);
+    TEST_ASSERT_EQUAL(testCost, 175);
 }
-void testSeminarFee()
-{
-    float semFee = 150.0;
-    printf("Enter total of seminar/conference fees: $");
-    printf("%.2f", semFee);
 
-    TEST_ASSERT_EQUAL_(150, semFee);
-}
