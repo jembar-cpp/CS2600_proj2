dbe948cd476c9548782a7e9aad26f9fd0eadde85
rctabares <98509950+rctabares@users.noreply.github.com>
2022-03-07 14:14:23 -0800

    edited miscExpenses to have a method

--- miscExpenses.c
+++ miscExpenses.c
@@ -8,49 +8,31 @@
 #include <stdio.h>
 
 
-int main()
+void getHotelExpenses(float hotelFee, int nights, float semFee)
 {
 
     // Declare variables needed
-    float totalSaved, totalExcess;
+    float totalHotelExpenses;
     float nights, hotelFee, semFee, totalHotelCost;
     float allowedHotelCost;
     float allowedPerNight = 90;
-
-    // Asks user for input of hotel fee per night, and total amount of nights at hotel
-    printf("Enter the hotel fee per night: $");
-    scanf("%f", &hotelFee);
-    
-    printf("Enter total nights at hotel: ");
-    scanf("%f", &nights);
-
-    printf("Enter total of seminar/conference fees: $");
-    scanf("%f", &semFee);
-
     // Calculates costs of both allowed total and total businessperson spent
     totalHotelCost = hotelFee * nights;
     allowedHotelCost = allowedPerNight * nights;
-
+    
     
     // Checks for hotelFee only, so rest of calcs are easier to check 
     if (hotelFee <= 89.99)
     {
-        totalSaved = allowedHotelCost - totalHotelCost;
-        printf("You saved: $%.2f\n", totalSaved);
-        printf("Total Cost with seminar: $%.2f\n", (totalSaved + semFee));
+        totalHotelExpenses = (allowedHotelCost - totalHotelCost) + semFee;
     }
     else if (hotelFee >= 90.99)
     {
-        totalExcess = fabs(totalHotelCost - allowedHotelCost);
-        printf("total cost is more than allowed, so we over spent: $%.2f\n", totalExcess);
-        printf("Total Cost with seminar: $%.2f\n", (totalExcess + semFee));
+        totalHotelExpenses = fabs(totalHotelCost - allowedHotelCost) + semFee;
     }
     else
     {  
-        printf("The company gave you the exact amount you needed! (for hotels)\n");
-        printf("Total Cost for hotel and seminar: $%.2f\n", semFee);
+        totalHotelExpenses = semFee;
     }
-    
-    
     return 0;
 }
