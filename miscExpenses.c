0ace3b65199288107c57764370f7dcc4d9de5bca
rctabares <98509950+rctabares@users.noreply.github.com>
2022-03-07 15:31:02 -0800

    added return statements

--- miscExpenses.c
+++ miscExpenses.c
@@ -8,7 +8,8 @@
 #include <stdio.h>
 
 
-void getHotelExpenses(float hotelFee, int nights)
+
+float getHotelExpenses()
 {
 
     // Declare variables needed
@@ -16,29 +17,41 @@ void getHotelExpenses(float hotelFee, int nights)
     float nights, hotelFee, totalHotelCost;
     float allowedHotelCost;
     float allowedPerNight = 90;
+    
+    
+    printf("Enter hotel cost per night: $%.2f");
+    scanf("%.2f", hotelFee);
+    printf("Enter number of nights staying: ");
+    scanf("%f", nights);
+
     // Calculates costs of both allowed total and total businessperson spent
     totalHotelCost = hotelFee * nights;
     allowedHotelCost = allowedPerNight * nights;
-    
-    
+
     // Checks for hotelFee only, so rest of calcs are easier to check 
     if (hotelFee <= 89.99)
     {
         totalHotelExpenses = (allowedHotelCost - totalHotelCost);
+        return totalHotelExpenses;
     }
     else if (hotelFee >= 90.99)
     {
         totalHotelExpenses = fabs(totalHotelCost - allowedHotelCost);
+        return totalHotelExpenses;
     }
     else
     {  
         totalHotelExpenses = 0;
+        return totalHotelExpenses;
     }
 
 }
 
-void getSemFee(float semFee)
+float getSemFee()
 {
-    float totalSemFee = semFee;
+    float semFee;
+    printf("Enter seminar/conference fees: ");
+    scanf("%.2f", semFee);
+    return semFee;
 }
 
