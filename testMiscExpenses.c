61a4e496e2ec9b51ad766d841a1e14b2198ed0a1
rctabares <98509950+rctabares@users.noreply.github.com>
2022-03-07 16:52:56 -0800

    edited methods to only check positive values

--- testMiscExpenses.c
+++ testMiscExpenses.c
@@ -10,38 +10,15 @@ void tearDown() {}
 
 void testHotelFees()
 {
+    float hotelFee = 50;
 
-    // Declare variables needed
-    float totalHotelCost, allowedHotelCost, testCost;
-    float nights = 5;
-    float hotelFee = 85.0;
-    float allowedPerNight = 90;
-
-    // Calculates costs of both allowed total and total businessperson spent
-    totalHotelCost = hotelFee * nights;
-    allowedHotelCost = allowedPerNight * nights;
-
-    
-    // Checks for hotelFee only, so rest of calcs are easier to check 
-    if (hotelFee <= 89.99)
-    {
-        testCost = (allowedHotelCost - totalHotelCost);
-    }
-    else if (hotelFee >= 90.99)
-    {
-        testCost = (totalHotelCost - allowedHotelCost);
-    }
-    else
-    {  
-        testCost = 0;
-    }
-
-    TEST_ASSERT_EQUAL(testCost, 25);
+    TEST_ASSERT_EQUAL(testCost, 50);
 }
 
 void testSemFee()
 {
-    float semFee = 150.0; 
+    float semFee = 150; 
+
     TEST_ASSERT_EQUAL(semFee, 150);
 }
 
