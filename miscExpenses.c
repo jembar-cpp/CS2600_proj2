8155132601dd8302739076509f8d7dbbb89b6a74
rctabares <98509950+rctabares@users.noreply.github.com>
2022-03-07 16:51:07 -0800

    edited methods to return hotelFee

--- miscExpenses.c
+++ miscExpenses.c
@@ -12,46 +12,33 @@
 float getHotelExpenses()
 {
 
-    // Declare variables needed
-    float totalHotelExpenses;
-    float nights, hotelFee, totalHotelCost;
-    float allowedHotelCost;
-    float allowedPerNight = 90;
+    float hotelFee;
     
+    printf("Enter hotel cost per night: $");
+    scanf("%f", &hotelFee);
     
-    printf("Enter hotel cost per night: $%.2f");
-    scanf("%.2f", hotelFee);
-    printf("Enter number of nights staying: ");
-    scanf("%f", nights);
-
-    // Calculates costs of both allowed total and total businessperson spent
-    totalHotelCost = hotelFee * nights;
-    allowedHotelCost = allowedPerNight * nights;
-
-    // Checks for hotelFee only, so rest of calcs are easier to check 
-    if (hotelFee <= 89.99)
-    {
-        totalHotelExpenses = (allowedHotelCost - totalHotelCost);
-        return totalHotelExpenses;
-    }
-    else if (hotelFee >= 90.99)
-    {
-        totalHotelExpenses = fabs(totalHotelCost - allowedHotelCost);
-        return totalHotelExpenses;
-    }
-    else
-    {  
-        totalHotelExpenses = 0;
-        return totalHotelExpenses;
+    while(hotelFee < 0) {
+        printf("Enter valid cost: $", hotelFee);
+        scanf("%f", &hotelFee);
     }
 
+    return hotelFee;
 }
+    
+    
 
 float getSemFee()
 {
     float semFee;
     printf("Enter seminar/conference fees: ");
-    scanf("%.2f", semFee);
+    scanf("%.2f", &semFee);
+
+    while (semFee < 0) {
+        printf("Enter valid seminal/conference fees: $%f");
+        scanf("%f", &semFee);
+    }
+
     return semFee;
 }
+
 
