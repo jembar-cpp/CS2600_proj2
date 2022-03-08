e2a457070a2bb4053bd58ddcdb6680a12e0aa399
Jeremy Embar <jembar@cpp.edu>
2022-03-07 17:22:29 -0800

    cleaned up file structure

--- tests/test_travelTime.c
+++ tests/test_travelTime.c
@@ -6,19 +6,19 @@ void setUp() {}
 void tearDown() {}
 
 void testGetNumDays() {
-    freopen("test_travelTime_input/numDays.txt", "r", stdin); // change standard input method
+    freopen("./tests/inputs/numDays.txt", "r", stdin); // change standard input method
     int numDays = getNumDays();
     TEST_ASSERT_EQUAL(numDays, 5);
 }
 
 void testGetDepartureTime() {
-    freopen("test_travelTime_input/departureTime.txt", "r", stdin);
+    freopen("./tests/inputs/departureTime.txt", "r", stdin);
     int departureTime = getDepartureTime();
     TEST_ASSERT_EQUAL(departureTime, 1130);
 }
 
 void testGetArrivalTime() {
-    freopen("test_travelTime_input/arrivalTime.txt", "r", stdin);
+    freopen("./tests/inputs/arrivalTime.txt", "r", stdin);
     int arrivalTime = getArrivalTime(1130, 3);
     TEST_ASSERT_EQUAL(arrivalTime, 1400);
 }
